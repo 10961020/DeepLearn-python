5a98af4bd9e6bbb30633729548e1aa64e633b926
tong.zhang <39048068+10961020@users.noreply.github.com>
2019-01-09 10:07:40 +0800

    Update Chapter_1.py

--- tensorflow/object_detection/Chapter_1.py
+++ tensorflow/object_detection/Chapter_1.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 '''
     目标检测打标签是一件特么痛苦的事情，此模块可以使用一个小型的标签数据训练好的模型 帮助你进行大规模的打标签的工作
+    首先你需要有一个小型的训练模型帮助你做打标签 这个模型自己训练喽 xml使用的模版我已给出 pbtxt如果你有小型模型的话 这个文件你也应该有的
+    代码中注释的部分都可以修改 我已给出作用
 '''
 # 将目标目录导入进来，这样才能执行下边两句导入命令
 # label_map_util,visualization_utils文件路径自行寻找 简单粗暴点 进入research目录全局搜这俩模块名
@@ -26,7 +28,7 @@ def load_image_into_numpy_array(image):
 
 # frozen_inference_graph.pb中保存的网络的结构跟数据
 # own_label_map.pbtxt中保存index到类别名的映射 需要通过pbtxt具体对应的类别是什么
-# 第三个全局是自训练的目录 路径名最后一层必须是 “img” 因为我懒得加这个在xml里 如果不是 打的标签不能用
+# 第三个全局是自训练的目录 路径名最后一层必须是 “img” 因为我懒得加这个在xml里 如果不是 打的标签好像不能用 日常迷信一下
 # 第四个保存的xml路径
 # oragn也是重要的参数 保存的自训练模版的文件 可以看到第二行的数据就是 img
 PATH_TO_FROZEN_GRAPH = 'C:/Users/Administrator/Desktop/car_position/frozen_inference_graph.pb'
@@ -49,6 +51,7 @@ category_index = label_map_util.create_category_index(categories)
 
 with detection_graph.as_default():
     with tf.Session() as sess:
+        tree = ET.ElementTree()
         for path, dirs, files in os.walk(TEST_IMAGE_PATHS):
             for image_path in files:
                 image_1 = Image.open(path+image_path)
@@ -93,14 +96,14 @@ with detection_graph.as_default():
                 # print("output_dict['num_detections']: ", output_dict['num_detections'])
 
                 image2 = Image.fromarray(image_np)
-                image2.save('1.jpg')
-                organ = "./xiugai.xml"
+                image2.save('1.jpg')  # 需要保存生成的图片的话  你给他个路径 推荐使用新的文件路径 名字就可以跟使用的文件名一样啦
+                organ = "./xiugai.xml"  # 这个是我给出的xml模型路径 跟程序放一块就好了不用修改
                 for i in range(output_dict['num_detections']):
-                    tree = ET.ElementTree()
+                    
                     tree.parse(organ)
-                    if output_dict['detection_scores'][i] < 0.8:
+                    if output_dict['detection_scores'][i] < 0.8: # 保留多大得分的选框 由你决定
                         break
-                    if i == 0:
+                    if i == 0:  # 如果图片中就一个目标
                         tree.find("filename").text = image_path
                         tree.find("path").text = path+image_path
                         tree.find("size/width").text = str(image2.size[0])
@@ -110,7 +113,7 @@ with detection_graph.as_default():
                         tree.find("object/bndbox/xmax").text = str(int(image2.size[0]*output_dict['detection_boxes'][i][3]))
                         tree.find("object/bndbox/ymax").text = str(int(image2.size[1]*output_dict['detection_boxes'][i][2]))
                         organ = TEST_XML_PATHS + os.path.splitext(image_path)[0] + '.xml'
-                    else:
+                    else:  # 在这个图片里发现多个目标 追加object
                         root = tree.getroot()
                         firstNode = ET.Element("object")
                         ET.SubElement(firstNode, "name").text = 'car'
@@ -122,7 +125,7 @@ with detection_graph.as_default():
                         ET.SubElement(twoNode, "ymin").text = str(int(image2.size[1]*output_dict['detection_boxes'][i][0]))
                         ET.SubElement(twoNode, "xmax").text = str(int(image2.size[0]*output_dict['detection_boxes'][i][3]))
                         ET.SubElement(twoNode, "ymax").text = str(int(image2.size[1]*output_dict['detection_boxes'][i][2]))
-                        root.append(firstNode)
+                        root.append(firstNode)  # 追加到xml文件 如果有多的目标框
                     tree.write(organ)
             break
 
