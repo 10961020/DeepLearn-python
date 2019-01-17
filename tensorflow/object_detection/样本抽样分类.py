4aa2b88686d06b63698053449a25c29b206af500
tong.zhang <39048068+10961020@users.noreply.github.com>
2019-01-17 09:23:35 +0800

    Update 样本抽样分类.py

--- tensorflow/object_detection/样本抽样分类.py
+++ tensorflow/object_detection/样本抽样分类.py
@@ -5,20 +5,21 @@
 '''
     把文件放置在对应位置
     运行此程序完成对训练集，测试集7/3分
+    生成txt文件  train.txt val.txt 
+    为目标检测
 '''
 
 import os
-file_dir = 'C:/Users/Administrator/Desktop/car_position/category_aircraft/VOC2007/Annotations/'
-files_dir = 'C:/Users/Administrator/Desktop/car_position/category_aircraft/VOC2007/imageSets/Main/'
+file_dir = 'C:/Users/Administrator/Desktop/car_position/category_aircraft/VOC2007/Annotations/'  # 图片路径
+files_dir = 'C:/Users/Administrator/Desktop/car_position/category_aircraft/VOC2007/imageSets/Main/'  # txt生成路径
 value = 0
 with open(files_dir + 'aeroplane_val.txt', 'w') as f:
     pass
 with open(files_dir+'aeroplane_train.txt', 'w') as f:
     pass
 for root, dirs, files in os.walk(file_dir):
-    print(len(files)//10*7)
     for i in range(len(files)):
-        if value >= len(files)//10*3:
+        if value >= len(files)*0.6:
             with open(files_dir+'aeroplane_train.txt', 'a') as f:
                 f.write(files[i][:files[i].find('.')]+'\n')
         elif i % 2:
