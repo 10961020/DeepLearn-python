1eb198d5fb5da4ee4e1e16375270f2da2c7f064f
tong.zhang <39048068+10961020@users.noreply.github.com>
2019-05-17 12:10:59 +0800

    Update make_data.py

--- keras/车牌识别/基于大陆常见机动车车牌识别/make_data.py
+++ keras/车牌识别/基于大陆常见机动车车牌识别/make_data.py
@@ -14,27 +14,32 @@ import numpy as np
 
 
 output_file = '1'
-validation_percentage = 10
-test_percentage = 10
+validation_percentage = 20  # 以下虽然是8 1 1区分训练验证集 主要是为了尽量先填满 验证测试集 保证训练集可以被32整除 结果比例也会是8 1 1
+test_percentage = 20        # 如果训练每次迭代读取的数据不想使用32个 需要把此代码里32都替换掉 比例 如需调整 修改 .8-->到你想要的比例大小
 
 
 # 读取数据集并分割为训练数据，验证数据和测试数据
 def create_image_lists(testing_percentage, validating_percentage):
-
     training_images = []
     training_labels = []
     testing_images = []
     testing_labels = []
     validation_images = []
     validation_labels = []
-
+    len_images = len(glob.glob('D:/1/project/plate_shibie/可以使用的/*.jpg'))  # 本地车牌图片的绝对路径
+    len_images = len_images - 32 * (int(len_images * .8) // 32)  # 验证测试集总数  修改此处  .8
+    
     for file_name in glob.glob('D:/1/project/plate_shibie/可以使用的/*.jpg'):  # 本地车牌图片的绝对路径
         dir_name = os.path.basename(file_name)
         print(os.path.splitext(dir_name)[0][:7])
         img = cv2.imdecode(np.fromfile(file_name, dtype=np.uint8), cv2.IMREAD_COLOR)
-        img = cv2.resize(img, (132, 42))
+        img = cv2.resize(img, (132, 42))  # 网络模型输入格式大小 可以修改此处 对应训练代码输入也要调整
         # img_tensor = image.img_to_array(img)
         # img_tensor = np.expand_dims(img_tensor, axis=0)
+        if len(validation_labels+testing_labels) == len_images:  # 保证train集总数可以被32整除 
+            training_images.append(img)
+            training_labels.append(os.path.splitext(dir_name)[0][:7])
+            continue
 
         chance = np.random.randint(100)  # 随机  8 1 1比例分配训练验证测试集
         if chance < validating_percentage:
@@ -51,6 +56,9 @@ def create_image_lists(testing_percentage, validating_percentage):
     np.random.shuffle(training_images)
     np.random.set_state(state)
     np.random.shuffle(training_labels)
+    print('training_labels length:\t', len(training_labels))
+    print('validation_labels length:\t', len(validation_labels))
+    print('testing_labels length:\t', len(testing_labels))
     return np.asarray([training_images, training_labels,
                        validation_images, validation_labels,
                        testing_images, testing_labels])
@@ -62,8 +70,8 @@ def main():
 
 main()
 
-processed_data = np.load('1.npy')
-training_images = processed_data[0]
-print(type(training_images))
-X = np.array(training_images, dtype=np.uint8)
-print(X.shape)
+# processed_data = np.load('1.npy')
+# training_images = processed_data[0]
+# print(type(training_images))
+# X = np.array(training_images, dtype=np.uint8)
+# print(X.shape)
